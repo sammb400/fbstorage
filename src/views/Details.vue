cb19d797fea6a3030002691f245d2392b1d7d9e8
Samuel Mwai <55353644+samilito75@users.noreply.github.com>
2024-04-04 16:21:40 +0300

    a bit pretty

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="flex flex-wrap gap-8 dark:bg-slate-800 py-20">
+    <div class="flex flex-wrap gap-8 dark:bg-slate-800 py-20 justify-center ">
     
 
-<div class="" v-for="(user, index) in users" :key="index">
-    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <img v-if="user.image" :src="user.image" alt="" class="rounded-full w-40 h-40">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ user.name }} - {{ user.email }}</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">{{ user.phone }} - {{ user.department }} - {{ user.idNo }}</p>
-    
-    </a>
-</div>
+        <div v-for="(user, index) in users" :key="index" class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="flex flex-col items-center pb-10">
+                <img v-if="user.image" class="mt-8 w-40 h-36 mb-3 rounded-full shadow-lg" :src="user.image" alt="Bonnie image"/>
+                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ user.name }}</h5>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{ user.email }}</span>
+                <span class="text-sm text-gray-500 dark:text-gray-400">Department - {{ user.department }}</span>
+                <div class="flex mt-4 md:mt-6">
+                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Phone - {{ user.phone }}</a>
+                    <a href="#" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">ID - {{ user.idNo }}</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
