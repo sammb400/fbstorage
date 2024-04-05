d64a6506c2df3188f06b072d466adc05f80c559b
Wade Njung'e <43459655+blvckn9ght@users.noreply.github.com>
2024-04-05 15:06:00 +0300

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div>
 
 
-        <form  class="max-w-md mx-auto">
+        <form @submit.prevent="userInfo" class="max-w-md mx-auto">
             <div class="relative z-0 w-full mb-5 group">
                 <input type="text" v-model="name" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
                 <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Name</label>
@@ -18,7 +18,7 @@
         
             <div class="grid md:grid-cols-2 md:gap-6">
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="number" v-model="phoneNo" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" v-model="phoneNo"  name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
@@ -29,7 +29,7 @@
             <label class="max-w-md mx-auto block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
             <input @change="uploadImage"  type="file" class="max-w-md mx-auto block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" >
 
-            <button @click="userInfo" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             
         </form>
             
@@ -58,7 +58,8 @@ export default {
         idNo: "",
         phoneNo: '',
         department: '',
-        imageUrl:"",
+            imageUrl: "",
+        imageStore:[],
         };
   },
   methods: {
@@ -66,23 +67,28 @@ export default {
         console.log(this.email, this.name, this.idNo, this.phoneNo, this.department)
     },
     async uploadImage (e) {
-        const file = e.target.files[0];
-        const storage = getStorage();
-
-        const storageRef = ref(storage, 'documents/' + file.name);
+        this.imageStore = e.target.files[0];
         
-
-        try {
-                    const snapshot = await uploadBytes(storageRef, file);
-                    console.log('Uploaded!');
-                    const downloadUrl = await getDownloadURL(storageRef)
-                    console.log(downloadUrl)
-                    this.imageUrl = downloadUrl
-                } catch (error) {
-                    console.error(error);
-                }
     },
-    async userInfo () {
+      async userInfo() {
+
+          const storage = getStorage();
+
+          const storageRef = ref(storage, 'documents/' + this.email);
+
+
+          try {
+              const snapshot = await uploadBytes(storageRef, this.imageStore);
+              console.log('Uploaded!');
+              const downloadUrl = await getDownloadURL(storageRef)
+              console.log(downloadUrl)
+              this.imageUrl = downloadUrl
+          } catch (error) {
+              console.error(error);
+          }
+
+
+        
         try {
             const dataObj = {
                 name: this.name,
